43e2de011278749a29085788a30433648871719e
Greg Anderson <greg.1.anderson@greenknowe.org>
2016-10-11 16:44:36 -0700

    Dump environment variables, and run 'composer install' from the 'code' directory (repository / composer root), not the 'web' directory (drupal root).

--- web/private/scripts/deploy_product/setup.php
+++ web/private/scripts/deploy_product/setup.php
@@ -1,6 +1,9 @@
 <?php
 
-chdir(__DIR__ . '/../../..');
+print "\n====== Printing environment ======\n\n";
+passthru('printenv');
+
+chdir(__DIR__ . '/../../../..');
 $cwd = getcwd();
 print "\n====== Running 'composer install' in $cwd ======\n\n";
 passthru('composer install');
