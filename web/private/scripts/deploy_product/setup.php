4f580663e5d96f3ee9348c2c0ab48a64bf24366d
Greg Anderson <greg.1.anderson@greenknowe.org>
2016-10-12 14:13:33 -0700

    Remove extraneous stuff.

--- web/private/scripts/deploy_product/setup.php
+++ web/private/scripts/deploy_product/setup.php
@@ -1,8 +1,7 @@
 <?php
 
 $version = phpversion();
-print "\n====== Sleep for a minute. (phpversion: $version) ======\n\n";
-sleep(60);
+print "\n====== phpversion: $version ======\n\n";
 
 chdir(getenv('HOME') . '/code');
 $cwd = getcwd();
@@ -12,10 +11,3 @@ passthru('composer install  2>&1');
 
 print "\n====== Running 'composer drupal-scaffold' ======\n\n";
 passthru('composer drupal-scaffold  2>&1');
-
-print "\n====== Running 'git status' ======\n\n";
-passthru('git status  2>&1');
-
-print "\n====== Use the apcu hammer =======\n\n";
-apcu_delete('skip_resurrection_check');
-apcu_store('database_is_empty', 'ok', 300);
