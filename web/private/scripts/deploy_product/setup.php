8ae386854097509dce7331c6ac56300d6f0b41ee
Greg Anderson <greg.1.anderson@greenknowe.org>
2016-10-11 22:21:19 -0700

    Temporarily disable setup script.

--- web/private/scripts/deploy_product/setup.php
+++ web/private/scripts/deploy_product/setup.php
@@ -4,10 +4,10 @@ chdir(getenv('HOME') . '/code');
 $cwd = getcwd();
 
 print "\n====== Running 'composer install' in $cwd ======\n\n";
-passthru('composer install  2>&1');
+//passthru('composer install  2>&1');
 
 print "\n====== Running 'composer drupal-scaffold' ======\n\n";
-passthru('composer drupal-scaffold  2>&1');
+//passthru('composer drupal-scaffold  2>&1');
 
 print "\n====== Running 'git status' ======\n\n";
-passthru('git status  2>&1');
+//passthru('git status  2>&1');
