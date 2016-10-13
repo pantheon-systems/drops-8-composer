631a4d28585340b9da5a75d62d4b9da31e4f1b48
Greg Anderson <greg.1.anderson@greenknowe.org>
2016-10-12 21:21:13 -0700

    Disable composer steps in qs script.

--- web/private/scripts/deploy_product/setup.php
+++ web/private/scripts/deploy_product/setup.php
@@ -7,7 +7,7 @@ chdir(getenv('HOME') . '/code');
 $cwd = getcwd();
 
 print "\n====== Running 'composer install' in $cwd ======\n\n";
-passthru('composer install  2>&1');
+//passthru('composer install  2>&1');
 
 print "\n====== Running 'composer drupal-scaffold' ======\n\n";
-passthru('composer drupal-scaffold  2>&1');
+//passthru('composer drupal-scaffold  2>&1');
